de3062fb05b832ab6e1d8872806cb50cd72a4a01
sigma-site <gv_bacau@yahoo.com>
2026-07-15 11:55:36 +0300

    Update vapi-chat.js

--- chat/vapi-chat.js
+++ chat/vapi-chat.js
@@ -1,1 +1,19 @@
-chat/vapi-chat.js
+/*
+ * Vapi Chat Loader
+ * Version: 0.1
+ * https://sigmasecurity.ro/chat/vapi-chat.js
+ */
+
+(function () {
+    "use strict";
+
+    // Verifică dacă există configurația
+    if (!window.VAPI_CONFIG) {
+        console.error("VAPI_CONFIG nu este definit.");
+        return;
+    }
+
+    console.log("Vapi Chat Loader");
+    console.log(window.VAPI_CONFIG);
+
+})();
